66480d5c767ce79c6f28224a84f45b0f53b0cc8c
thein <t.hein@perview.de>
2024-09-29 13:28:40 +0200

    add docekr support

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 # mkdir hisss && cd hisss
 python3 -m venv venv
 source venv/bin/activate
-pip install fastapi==0.111.0 jinja2==3.1.4
-pip install passlib[bcrypt] python-jose
+pip install -r requirements.txt
